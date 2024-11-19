d6bbdce5abdb5bddc2cd7b6d726fd2a76d4bd3ff
unknown <singhsusna2003002@gmail.com>
2024-11-19 17:29:27 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 var a = 10
 console.log(a)
 const heading = React.createElement("div",{},[React.createElement('h1',{},"first div"), React.createElement("h1",{},"second div")])
