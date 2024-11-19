b9bb0177f0c36449d1af3d4932cd1a68bf4b849d
unknown <singhsusna2003002@gmail.com>
2024-11-19 16:54:25 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,5 @@
+var a = 10
+console.log(a)
+const heading = React.createElement("div",{},[React.createElement('h1',{},"first div"), React.createElement("h1",{},"second div")])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
