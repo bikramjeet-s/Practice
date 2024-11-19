458f8c9bfa66cc357086bf9d80c69396fe31f634
unknown <singhsusna2003002@gmail.com>
2024-11-19 19:41:26 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 var a = 10
 console.log(a)
-const heading = React.createElement("div",{},[React.createElement('h1',{},"first div"), React.createElement("h1",{},"second div")])
+const heading = (<div><h1>Hello World</h1><h1><p>{400+600}</p></h1></div>)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
+
+// Functional Component
+const Heading1 = () =>{
+    return (<div><h1>Hello World from functional component</h1><h1><p>{400+600}</p></h1></div>)
+}
+
+root.render(<Heading1/>)
